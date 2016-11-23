d65bf4d170003ab7ccf756ac865b7bf73cc002fb
Buhtoyarov Artem 🙈 <buhtoyarov1986@gmail.com>
2016-11-23 13:49:51 +0300

    a new attribute for easy access

--- lib/yandex_kassa_form/configuration.rb
+++ lib/yandex_kassa_form/configuration.rb
@@ -1,5 +1,5 @@
 module YandexKassaForm
   class Configuration
-    attr_accessor :shop_id, :shop_password, :check_order, :save_payment
+    attr_accessor :shop_id, :sc_id, :shop_password, :check_order, :save_payment
   end
 end
